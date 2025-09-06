f9f2f8ab468688c228a78bad60f8e59dca6fb77b
Rohit Jarariya <jarariyarohit171198@gmail.com>
2025-09-07 00:16:15 +0530

    SpringJDBC CRUD APP

---
+++ springjdbc_CRUD/src/test/java/springTest/springjdbc_CRUD/SpringjdbcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package springTest.springjdbc_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjdbcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
