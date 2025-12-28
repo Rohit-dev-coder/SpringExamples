de8a085abc03361290ec9a9e3d0d36f6cd90e66d
Rohit Jarariya <jarariyarohit171198@gmail.com>
2025-12-28 23:35:35 +0530

    Add EmailSenderEx1 Spring Boot project

---
+++ EmailSenderEx1/src/main/java/com/springTest/EmailSenderEx1/EmailSenderEx1Application.java
@@ -0,0 +1,13 @@
+package com.springTest.EmailSenderEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderEx1Application.class, args);
+	}
+
+}
---
+++ EmailSenderEx1/src/test/java/com/springTest/EmailSenderEx1/EmailSenderEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springTest.EmailSenderEx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSenderEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
